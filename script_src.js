c9a45e3c4215e667e67492102f86e799911dcbda
Harry Park <phg98@naver.com>
2020-08-20 22:47:42 +0900

    핑 기능 추가

--- script_src.js
+++ script_src.js
@@ -1,8 +1,8 @@
-var url = "http://127.0.0.1:3000/users";
+var url = "http://127.0.0.1:3000";
 
 function formSubmit(event) {
   var request = new XMLHttpRequest();
-  request.open('POST', url, true);
+  request.open('POST', url+'/users', true);
   request.onload = function () { // request successful
     // we can use server response to our request now
     console.log(request.responseText);
@@ -45,7 +45,38 @@ function SearchServer() {
       }
     }
   }
-  request.open('GET', url+'/'+serverName, true);
+  request.open('GET', url+'/users'+'/'+serverName);
+  request.onload = function () { // request successful
+    // we can use server response to our request now
+    console.log(request.responseText);
+  };
+
+  request.onerror = function () {
+    // request failed
+  };
+
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send();
+  event.preventDefault();
+} 
+
+function PingServer() {
+  var serverName = document.getElementById('serverName').value
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {        
+        if (JSON.parse(request.responseText).message === "Got ping")
+          alert(`전송하였습니다.`)
+        else
+          alert('전송결과 실패입니다.')
+      } else {
+        alert('전송에 실패하였습니다.');
+      }
+    }
+  }
+  request.open('GET', url+'/ping'+'/'+serverName);
   request.onload = function () { // request successful
     // we can use server response to our request now
     console.log(request.responseText);
