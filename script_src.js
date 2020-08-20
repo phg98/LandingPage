b4929cbf36d412a3590ae789e51b3b0b87758647
Harry Park <hg98.park@hanwha.com>
2020-08-20 17:59:22 +0900

    다른 피씨에서 작업하기 위해 작업중에 커밋

--- script_src.js
+++ script_src.js
@@ -1,5 +1,6 @@
+var url = "http://127.0.0.1:3000/users";
+
 function formSubmit(event) {
-  var url = "http://127.0.0.1:3000/users";
   var request = new XMLHttpRequest();
   request.open('POST', url, true);
   request.onload = function () { // request successful
@@ -28,3 +29,31 @@ function attachFormSubmitEvent(formId) {
 }
 
 attachFormSubmitEvent('AddServer')
+
+function SearchServer() {
+  var serverName = document.getElementById('serverName').textContent
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        alert(request.responseText);
+      } else {
+        alert('request에 뭔가 문제가 있어요.');
+      }
+    }
+  }
+  request.open('GET', url+'/'+serverName, true);
+  request.onload = function () { // request successful
+    // we can use server response to our request now
+    console.log(request.responseText);
+  };
+
+  request.onerror = function () {
+    // request failed
+  };
+
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send();
+  event.preventDefault();
+} 
